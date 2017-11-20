d18cba937e10382bf28a579ffced52e360fe4f7a
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2017-11-20 11:16:24 -0500

    helper css v.1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp') ;
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+// SASS
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('sass-dev', function () {
+  return gulp.src('./sass/app.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./css'));
+
+});
+
+gulp.task('sass-prod', function () {
+  return gulp.src('./sass/app.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css'));
+});
+
+
