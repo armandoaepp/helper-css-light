9f01802e6758d544f2efe7b9c8fbf5606ad63221
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2017-12-28 12:11:22 -0500

    ok for production

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ var rename = require('gulp-rename');
 
 // SASS
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+// var sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('sass-dev', function () {
   return gulp.src('./sass/helper-css.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('./maps'))
+    // .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./css'));
 
 });
