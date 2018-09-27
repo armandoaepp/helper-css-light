3db02a8f418ed1a0759349c9533138ab553e2307
Armando Enrique PIsfil Puemape <armandoaepp@gmail.com>
2018-09-26 23:30:58 -0500

    version 2.4.0

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+
+  entry: {
+    // 'helper-css': './scss/helper-css.scss',
+    'helper-bootstrap': './scss/helper-bootstrap.scss',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './css')
+  },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader",
+                options: {
+                    // includePaths: ["node_modules"]
+                    // includePaths: require('path').resolve(__dirname, 'node_modules')
+                }
+            }]
+        }]
+    }
+};
