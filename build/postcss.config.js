4dd842c5e880805910e6c633d038a04acd0458a6
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2022-03-13 18:51:15 -0500

    actualizando paquetes

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,14 +1,22 @@
 'use strict'
 
-module.exports = (ctx) => ({
+/* module.exports = (ctx) => ({
   map: ctx.file.dirname.includes('examples') ? false : {
     inline: false,
-    annotation: true,
-    sourcesContent: true
+    annotation: true
+    // ,sourcesContent: true
   },
   plugins: {
     autoprefixer: {
       cascade: false
     }
   }
-})
+}) */
+
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      cascade: false
+    }
+  }
+}
